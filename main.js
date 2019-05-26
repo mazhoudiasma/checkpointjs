900e9417bdaa59ce04a1ab5284e4947701b209ff
mazhoudiasma <50797814+mazhoudiasma@users.noreply.github.com>
2019-05-26 16:39:06 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+    
+ console.log('valide')
+
+ 
+ 
+ function italic()
+   {
+   para.style.fontStyle='italic';
+   }
+    
+function bold()
+   {
+   document.getElementById('texte').style.fontWeight='bold';
+   }
+    
+   function underline()
+   {
+   document.getElementById('texte').style.textDecoration='underline';
+   }
+    
+  
+    
+ 
+ 
